51f1d7feab866d8eec3d70f3fd7798b42563e138
TouKhen <jack.lecomtes@gmail.com>
2023-01-06 13:46:53 +0100

    shooting done; movement done, enemies almost done

--- js/app.js
+++ js/app.js
@@ -1,269 +1,383 @@
 import * as THREE from "three";
 import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import * as CANNON from "cannon-es";
+// import { threeToCannon, ShapeType } from "three-to-cannon";
 
-let cube, controls, raycaster;
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-let canJump = false;
+import Stats from "https://unpkg.com/three@0.122.0/examples/jsm/libs/stats.module.js";
+import { PointerLockControlsCannon } from "./PointerLockControlsCannon.js";
 
-let prevTime = performance.now();
-const velocity = new THREE.Vector3();
-const direction = new THREE.Vector3();
-const pointer = new THREE.Vector2();
+/**
+ * Example of a really barebones version of a fps game.
+ */
 
-const blocker = document.getElementById("blocker");
+// three.js variables
+let camera, scene, renderer, stats;
+let material;
+
+// cannon.js variables
+let world;
+let controls;
+const timeStep = 1 / 60;
+let lastCallTime = performance.now();
+let sphereShape;
+let sphereBody;
+let physicsMaterial;
+let hitCooldown = false;
+let boxHit = 0;
+let hitText = document.querySelector("#one-point");
+let score = document.querySelector("#score span");
+const balls = [];
+const ballMeshes = [];
+const boxes = [];
+const boxMeshes = [];
+const boxBodies = [];
+
 const instructions = document.getElementById("instructions");
 
-// Camera
-const aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(75, aspect, 1, 5000);
+initThree();
+initCannon();
+initPointerLock();
+animate();
 
-// Scene
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x0f1020);
+function initThree() {
+  // Camera
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-controls = new PointerLockControls(camera, document.body);
+  // Scene
+  scene = new THREE.Scene();
+  scene.fog = new THREE.Fog(0x5ee7fa, 0, 500);
 
-instructions.addEventListener("click", function () {
-  controls.lock();
-});
+  // Renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(scene.fog.color);
 
-controls.addEventListener("lock", function () {
-  instructions.style.display = "none";
-  blocker.style.display = "none";
-});
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-controls.addEventListener("unlock", function () {
-  blocker.style.display = "block";
-  instructions.style.display = "";
-});
+  document.body.appendChild(renderer.domElement);
 
-scene.add(controls.getObject());
+  // Stats.js
+  stats = new Stats();
+  document.body.appendChild(stats.dom);
 
-// Renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  // Lights
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
 
-// Light
-const light = new THREE.PointLight(0xffffff, 2, 500);
-light.position.set(-5, 15, 0);
-scene.add(light);
+  const spotlight = new THREE.SpotLight(0xffffff, 0.9, 0, Math.PI / 4, 1);
+  spotlight.position.set(10, 40, 20);
+  spotlight.target.position.set(0, 0, 0);
 
-// Ambient light
-const ambient = new THREE.AmbientLight(0xffffff, 0.2);
-scene.add(ambient);
+  spotlight.castShadow = true;
 
-// red : 0xff0000
-// purple : 0x7353ba;
+  spotlight.shadow.camera.near = 10;
+  spotlight.shadow.camera.far = 100;
+  spotlight.shadow.camera.fov = 30;
 
-// Texture
+  // spotlight.shadow.bias = -0.0001
+  spotlight.shadow.mapSize.width = 2048;
+  spotlight.shadow.mapSize.height = 2048;
 
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load(
-  "./assets/pdpnew.png",
-  (texture) => {
-    buildMesh(texture);
-  },
-  undefined,
-  (error) => {
-    console.log(error);
+  scene.add(spotlight);
+
+  // Generic material
+  material = new THREE.MeshLambertMaterial({ color: 0xdddddd });
+
+  // Floor
+  // const floorGeometry = new THREE.PlaneBufferGeometry(300, 300, 100, 100);
+  // floorGeometry.rotateX(-Math.PI / 2);
+  // const floor = new THREE.Mesh(floorGeometry, material);
+  // floor.receiveShadow = true;
+  // scene.add(floor);
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function generateBoxes(nbBoxes) {
+  // Add boxes both in cannon.js and three.js
+  const halfExtents = new CANNON.Vec3(1, 1, 1);
+  const boxShape = new CANNON.Box(halfExtents);
+  const boxGeometry = new THREE.BoxBufferGeometry(
+    halfExtents.x * 2,
+    halfExtents.y * 2,
+    halfExtents.z * 2
+  );
+  const gltfLoader = new GLTFLoader();
+  // Generic material
+  material = new THREE.MeshLambertMaterial({ color: 0xdddddd });
+
+  for (let i = 0; i < nbBoxes; i++) {
+    gltfLoader.load(
+      // resource URL
+      "../assets/models/box.glb",
+      // called when the resource is loaded
+      function (object3D) {
+        // object3D.scene.scale.set(1, 1, 1);
+        const boxBody = new CANNON.Body({ mass: 5 });
+        boxBody.addShape(boxShape);
+        const boxMesh = object3D.scene;
+
+        const x = (Math.random() - 0.5) * 40;
+        const y = 5;
+        const z = (Math.random() - 0.5) * 40;
+
+        boxBody.position.set(x, y, z);
+        boxMesh.position.copy(boxBody.position);
+
+        boxMesh.castShadow = true;
+        boxMesh.receiveShadow = true;
+
+        boxMesh.name = boxBody.id;
+
+        world.addBody(boxBody);
+        scene.add(boxMesh);
+        boxes.push(boxBody);
+        boxMeshes.push(boxMesh);
+        boxBodies.push(boxBody);
+      },
+      // called while loading is progressing
+      function (xhr) {
+        console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      function (error) {
+        console.log("An error happened");
+      }
+    );
   }
-);
+}
 
-// TorusKnot
+function initCannon() {
+  world = new CANNON.World();
 
-function buildMesh(texture) {
-  const geometry = new THREE.BoxBufferGeometry();
-  const material = new THREE.MeshPhongMaterial({
-    color: 0xffffff,
-    map: texture,
+  // Tweak contact properties.
+  // Contact stiffness - use to make softer/harder contacts
+  world.defaultContactMaterial.contactEquationStiffness = 1e9;
+
+  // Stabilization time in number of timesteps
+  world.defaultContactMaterial.contactEquationRelaxation = 4;
+
+  const solver = new CANNON.GSSolver();
+  solver.iterations = 7;
+  solver.tolerance = 0.1;
+  world.solver = new CANNON.SplitSolver(solver);
+  // use this to test non-split solver
+  // world.solver = solver
+
+  world.gravity.set(0, -20, 0);
+
+  // Create a slippery material (friction coefficient = 0.0)
+  physicsMaterial = new CANNON.Material("physics");
+  const physics_physics = new CANNON.ContactMaterial(
+    physicsMaterial,
+    physicsMaterial,
+    {
+      friction: 0.0,
+      restitution: 0.3,
+    }
+  );
+
+  // We must add the contact materials to the world
+  world.addContactMaterial(physics_physics);
+
+  // Create the user collision sphere
+  const radius = 1.3;
+  sphereShape = new CANNON.Sphere(radius);
+  sphereBody = new CANNON.Body({ mass: 5, material: physicsMaterial });
+  sphereBody.addShape(sphereShape);
+  sphereBody.position.set(0, 5, 0);
+  sphereBody.linearDamping = 0.9;
+  world.addBody(sphereBody);
+
+  // Create the ground plane
+  const gltfLoader = new GLTFLoader();
+
+  gltfLoader.load(
+    // resource URL
+    "../assets/models/map.glb",
+    // called when the resource is loaded
+    function (gltf) {
+      gltf.scene.scale.set(0.5, 0.5, 0.5);
+      scene.add(gltf.scene);
+    },
+    // called while loading is progressing
+    function (xhr) {
+      console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+    },
+    // called when loading has errors
+    function (error) {
+      console.log("An error happened");
+    }
+  );
+
+  const groundShape = new CANNON.Plane();
+  const groundBody = new CANNON.Body({ mass: 0, material: physicsMaterial });
+  groundBody.addShape(groundShape);
+  groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+  world.addBody(groundBody);
+
+  generateBoxes(5);
+
+  // The shooting balls
+  const shootVelocity = 15;
+  const ballShape = new CANNON.Sphere(0.1);
+  const ballGeometry = new THREE.SphereBufferGeometry(ballShape.radius, 32, 32);
+
+  // Returns a vector pointing the the diretion the camera is at
+  function getShootDirection() {
+    const vector = new THREE.Vector3(0, 0, 1);
+    vector.unproject(camera);
+    const ray = new THREE.Ray(
+      sphereBody.position,
+      vector.sub(sphereBody.position).normalize()
+    );
+    return ray.direction;
+  }
+
+  window.addEventListener("click", (event) => {
+    if (!controls.enabled) {
+      return;
+    }
+
+    const ballBody = new CANNON.Body({ mass: 1 });
+    ballBody.addShape(ballShape);
+    const ballMesh = new THREE.Mesh(ballGeometry, material);
+
+    ballMesh.castShadow = true;
+    ballMesh.receiveShadow = true;
+
+    world.addBody(ballBody);
+    scene.add(ballMesh);
+    balls.push(ballBody);
+    ballMeshes.push(ballMesh);
+
+    const shootDirection = getShootDirection();
+    ballBody.velocity.set(
+      shootDirection.x * shootVelocity,
+      shootDirection.y * shootVelocity,
+      shootDirection.z * shootVelocity
+    );
+
+    // Move the ball outside the player sphere
+    const x =
+      sphereBody.position.x +
+      shootDirection.x * (sphereShape.radius * 1.02 + ballShape.radius);
+    const y =
+      sphereBody.position.y +
+      shootDirection.y * (sphereShape.radius * 1.02 + ballShape.radius);
+    const z =
+      sphereBody.position.z +
+      shootDirection.z * (sphereShape.radius * 1.02 + ballShape.radius);
+    ballBody.position.set(x, y, z);
+    ballMesh.position.copy(ballBody.position);
   });
 }
 
-// Map
+function initPointerLock() {
+  controls = new PointerLockControlsCannon(camera, sphereBody);
+  scene.add(controls.getObject());
 
-const gltfLoader = new GLTFLoader();
+  instructions.addEventListener("click", () => {
+    controls.lock();
+  });
 
-gltfLoader.load(
-  // resource URL
-  "../assets/models/map.glb",
-  // called when the resource is loaded
-  function (gltf) {
-    gltf.scene.scale.set(3.5, 3.5, 3.5);
-    scene.add(gltf.scene);
-  },
-  // called while loading is progressing
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  // called when loading has errors
-  function (error) {
-    console.log("An error happened");
-  }
-);
+  controls.addEventListener("lock", () => {
+    controls.enabled = true;
+    instructions.style.display = "none";
+  });
 
-// Gun
-
-gltfLoader.load(
-  // resource URL
-  "../assets/models/gun.glb",
-  // called when the resource is loaded
-  function (gltf) {
-    gltf.scene.scale.set(10, 10, 10);
-    gltf.scene.name = "gun";
-    scene.add(gltf.scene);
-  },
-  // called while loading is progressing
-  function (xhr) {
-    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-  },
-  // called when loading has errors
-  function (error) {
-    console.log("An error happened");
-  }
-);
-
-// Controls
-scene.add(controls.getObject());
-
-const onKeyDown = function (event) {
-  switch (event.code) {
-    case "ArrowUp":
-    case "KeyW":
-      moveForward = true;
-      break;
-
-    case "ArrowLeft":
-    case "KeyA":
-      moveLeft = true;
-      break;
-
-    case "ArrowDown":
-    case "KeyS":
-      moveBackward = true;
-      break;
-
-    case "ArrowRight":
-    case "KeyD":
-      moveRight = true;
-      break;
-
-    case "Space":
-      if (canJump === true) velocity.y += 175;
-      canJump = false;
-      break;
-  }
-};
-
-const onKeyUp = function (event) {
-  switch (event.code) {
-    case "ArrowUp":
-    case "KeyW":
-      moveForward = false;
-      break;
-
-    case "ArrowLeft":
-    case "KeyA":
-      moveLeft = false;
-      break;
-
-    case "ArrowDown":
-    case "KeyS":
-      moveBackward = false;
-      break;
-
-    case "ArrowRight":
-    case "KeyD":
-      moveRight = false;
-      break;
-  }
-};
-
-document.addEventListener("keydown", onKeyDown);
-document.addEventListener("keyup", onKeyUp);
-
-raycaster = new THREE.Raycaster(
-  new THREE.Vector3(),
-  new THREE.Vector3(0, -1, 0),
-  0,
-  10
-);
-
-camera.position.setY(2);
-
-function onPointerMove(event) {
-  // calculate pointer position in normalized device coordinates
-  // (-1 to +1) for both components
-
-  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
-  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  controls.addEventListener("unlock", () => {
+    controls.enabled = false;
+    instructions.style.display = null;
+  });
 }
 
-const objects = scene.children;
+function animate() {
+  requestAnimationFrame(animate);
 
-function render() {
-  //   if (cube) cube.rotation.y += 0.005;
+  const time = performance.now() / 1000;
+  const dt = time - lastCallTime;
+  lastCallTime = time;
 
-  const time = performance.now();
+  if (controls.enabled) {
+    world.step(timeStep, dt);
 
-  if (controls.isLocked === true) {
-    raycaster.ray.origin.copy(controls.getObject().position);
-    raycaster.ray.origin.y -= 10;
+    // Update ball positions
+    for (let i = 0; i < balls.length; i++) {
+      ballMeshes[i].position.copy(balls[i].position);
+      ballMeshes[i].quaternion.copy(balls[i].quaternion);
+      for (let j = 0; j < boxes.length; j++) {
+        if (
+          balls[i].position.x < boxes[j].position.x + 1 &&
+          balls[i].position.x > boxes[j].position.x - 1 &&
+          balls[i].position.y < boxes[j].position.y + 1 &&
+          balls[i].position.y > boxes[j].position.y - 1 &&
+          balls[i].position.z < boxes[j].position.z + 1 &&
+          balls[i].position.z > boxes[j].position.z - 1
+        ) {
+          for (const object of world.bodies) {
+            if (
+              object.id == boxMeshes[j].name &&
+              object.shapes[0].type === 4 &&
+              hitCooldown === false
+            ) {
+              scene.remove(boxMeshes[j]);
+              world.removeBody(object);
 
-    const intersections = raycaster.intersectObjects(objects, false);
+              boxHit += 1;
+              score.innerHTML++;
 
-    const onObject = intersections.length > 0;
+              if (boxHit === 5) {
+                generateBoxes(5);
+                boxHit = 0;
+              }
 
-    const delta = (time - prevTime) / 1000;
+              hitText.classList.toggle("hidden");
+              hitText.classList.toggle("transform-up");
+              setTimeout(() => {
+                hitText.classList.toggle("hidden");
+                hitText.classList.toggle("transform-up");
+              }, 650);
 
-    velocity.x -= velocity.x * 10.0 * delta;
-    velocity.z -= velocity.z * 10.0 * delta;
+              hitCooldown = true;
 
-    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-
-    direction.z = Number(moveForward) - Number(moveBackward);
-    direction.x = Number(moveRight) - Number(moveLeft);
-    direction.normalize(); // this ensures consistent movements in all directions
-
-    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
-    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
-
-    if (onObject === true) {
-      velocity.y = Math.max(0, velocity.y);
-      canJump = true;
+              setTimeout(() => {
+                hitCooldown = false;
+              }, 660);
+            }
+          }
+        }
+      }
     }
 
-    controls.moveRight(-velocity.x * delta);
-    controls.moveForward(-velocity.z * delta);
-
-    controls.getObject().position.y += velocity.y * delta; // new behavior
-
-    if (controls.getObject().position.y < 10) {
-      velocity.y = 0;
-      controls.getObject().position.y = 10;
-
-      canJump = true;
+    // Update box positions
+    for (let i = 0; i < boxes.length; i++) {
+      boxMeshes[i].position.copy(boxes[i].position);
+      boxMeshes[i].quaternion.copy(boxes[i].quaternion);
     }
   }
 
-  for (const object of scene.children) {
-    if (object.name === "gun") {
-      object.position.x = camera.position.x;
-      object.position.y = camera.position.y - 1.5;
-      object.position.z = camera.position.z + 2;
-
-      object.rotateY(camera.rotation.y);
-    }
-  }
-
-  prevTime = time;
-
+  controls.update(dt);
   renderer.render(scene, camera);
-  requestAnimationFrame(render);
+  stats.update();
 }
 
-render();
+// console.log(ballMeshes);
+// console.log(boxMeshes);
+// console.log(scene.children);
+// console.log(world);
+// console.log(boxBodies);
+// console.log(boxes);
--- js/test.js
+++ js/test.js
@@ -1,52 +1,267 @@
 import * as THREE from "three";
+import { PointerLockControls } from "three/addons/controls/PointerLockControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-const scene = new THREE.Scene();
-scene.backgroundColor = new THREE.Color(0x70877f);
+let cube, controls, raycaster;
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
 
-for (let i = 0; i < 200; i++) {
-  let compteur = Math.floor(Math.random() * (5 - 1) + 1);
-  console.log(compteur);
-  if (compteur === 1) {
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-  }
-  if (compteur === 2) {
-    const geometry = new THREE.CapsuleGeometry(1, 1, 4, 8);
-    const material = new THREE.MeshBasicMaterial({ color: 0xee7224 });
-    const capsule = new THREE.Mesh(geometry, material);
-    scene.add(capsule);
-  }
-  if (compteur === 3) {
-    const geometry = new THREE.RingGeometry(1, 5, 32);
-    const material = new THREE.MeshBasicMaterial({
-      color: "0c6291",
-      side: THREE.DoubleSide,
-    });
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-  }
-  if (compteur === 4) {
-    const geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
-    const material = new THREE.MeshBasicMaterial({ color: "b5d6d6" });
-    const torusKnot = new THREE.Mesh(geometry, material);
-    scene.add(torusKnot);
-  }
-}
-//camera
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+const pointer = new THREE.Vector2();
+
+const blocker = document.getElementById("blocker");
+const instructions = document.getElementById("instructions");
+
+// Camera
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, aspect, 1, 5000);
-// scene.add(camera);
-camera.position.setZ(30);
 
-//render
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x0f1020);
+
+controls = new PointerLockControls(camera, document.body);
+
+instructions.addEventListener("click", function () {
+  controls.lock();
+});
+
+controls.addEventListener("lock", function () {
+  instructions.style.display = "none";
+  blocker.style.display = "none";
+});
+
+controls.addEventListener("unlock", function () {
+  blocker.style.display = "block";
+  instructions.style.display = "";
+});
+
+scene.add(controls.getObject());
+
+// Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Light
+const light = new THREE.PointLight(0xffffff, 2, 500);
+light.position.set(-5, 15, 0);
+scene.add(light);
+
+// Ambient light
+const ambient = new THREE.AmbientLight(0xffffff, 0.2);
+scene.add(ambient);
+
+// red : 0xff0000
+// purple : 0x7353ba;
+
+// Texture
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load(
+  "./assets/pdpnew.png",
+  (texture) => {
+    buildMesh(texture);
+  },
+  undefined,
+  (error) => {
+    console.log(error);
+  }
+);
+
+// TorusKnot
+
+function buildMesh(texture) {
+  const geometry = new THREE.BoxBufferGeometry();
+  const material = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    map: texture,
+  });
+}
+
+// Map
+
+const gltfLoader = new GLTFLoader();
+
+gltfLoader.load(
+  // resource URL
+  "../assets/models/map.glb",
+  // called when the resource is loaded
+  function (gltf) {
+    gltf.scene.scale.set(3.5, 3.5, 3.5);
+    scene.add(gltf.scene);
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+// Gun
+
+gltfLoader.load(
+  // resource URL
+  "../assets/models/gun.glb",
+  // called when the resource is loaded
+  function (gltf) {
+    gltf.scene.scale.set(10, 10, 10);
+    gltf.scene.name = "gun";
+    scene.add(gltf.scene);
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+// Controls
+scene.add(controls.getObject());
+
+const onKeyDown = function (event) {
+  switch (event.code) {
+    case "ArrowUp":
+    case "KeyW":
+      moveForward = true;
+      break;
+
+    case "ArrowLeft":
+    case "KeyA":
+      moveLeft = true;
+      break;
+
+    case "ArrowDown":
+    case "KeyS":
+      moveBackward = true;
+      break;
+
+    case "ArrowRight":
+    case "KeyD":
+      moveRight = true;
+      break;
+
+    case "Space":
+      if (canJump === true) velocity.y += 175;
+      canJump = false;
+      break;
+  }
+};
+
+const onKeyUp = function (event) {
+  switch (event.code) {
+    case "ArrowUp":
+    case "KeyW":
+      moveForward = false;
+      break;
+
+    case "ArrowLeft":
+    case "KeyA":
+      moveLeft = false;
+      break;
+
+    case "ArrowDown":
+    case "KeyS":
+      moveBackward = false;
+      break;
+
+    case "ArrowRight":
+    case "KeyD":
+      moveRight = false;
+      break;
+  }
+};
+
+document.addEventListener("keydown", onKeyDown);
+document.addEventListener("keyup", onKeyUp);
+
+raycaster = new THREE.Raycaster(
+  new THREE.Vector3(),
+  new THREE.Vector3(0, -1, 0),
+  0,
+  10
+);
+
+camera.position.setY(2);
+
+function onPointerMove(event) {
+  // calculate pointer position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+
+const objects = scene.children;
+
 function render() {
-  //  controls.update();
+  //   if (cube) cube.rotation.y += 0.005;
+
+  const time = performance.now();
+
+  if (controls.isLocked === true) {
+    raycaster.ray.origin.copy(controls.getObject().position);
+    raycaster.ray.origin.y -= 10;
+
+    const intersections = raycaster.intersectObjects(objects, false);
+
+    const onObject = intersections.length > 0;
+
+    const delta = (time - prevTime) / 1000;
+
+    velocity.x -= velocity.x * 10.0 * delta;
+    velocity.z -= velocity.z * 10.0 * delta;
+
+    velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+    direction.z = Number(moveForward) - Number(moveBackward);
+    direction.x = Number(moveRight) - Number(moveLeft);
+    direction.normalize(); // this ensures consistent movements in all directions
+
+    if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+    if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+    if (onObject === true) {
+      velocity.y = Math.max(0, velocity.y);
+      canJump = true;
+    }
+
+    controls.moveRight(-velocity.x * delta);
+    controls.moveForward(-velocity.z * delta);
+
+    controls.getObject().position.y += velocity.y * delta; // new behavior
+
+    if (controls.getObject().position.y < 10) {
+      velocity.y = 0;
+      controls.getObject().position.y = 10;
+
+      canJump = true;
+    }
+  }
+
+  for (const object of scene.children) {
+    if (object.name === "gun") {
+      object.position.x = camera.position.x;
+      object.position.y = camera.position.y - 1.5;
+      object.position.z = camera.position.z + 2;
+
+      object.rotateY(camera.rotation.y);
+    }
+  }
+
+  prevTime = time;
+
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
